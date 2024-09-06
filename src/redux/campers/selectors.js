c0ac4075ae05666b04a3dd04ae0bfaa60cf0a6b1
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-09-06 23:34:42 +0300

    CatalogePage filter

--- src/redux/campers/selectors.js
+++ src/redux/campers/selectors.js
@@ -8,11 +8,34 @@ export const selectError = state => state.campers.error;
 export const selectFilteredCampers = createSelector(
   [selectCampers, selectFilters],
   (items, filters) => {
-    const filteredCampers = items.filter(
-      camper =>
-        camper.location.toLowerCase().includes(filters.location.toLowerCase())
+    const filteredCampers = items.filter(camper => {
+      const filterLocation = camper.location
+        .toLowerCase()
+        .includes(filters.location.toLowerCase());
+      const filterAC = filters.AC ? filters.AC === camper.AC : true;
+      const filterTransmission = filters.transmission[0]
+        ? filters.transmission[0] === camper.transmission
+        : true;
+      const filterKitchen = filters.kitchen
+        ? filters.kitchen === camper.kitchen
+        : true;
+      const filterTv = filters.tv ? filters.tv === camper.tv : true;
+      const filterBathroom = filters.bathroom
+        ? filters.bathroom === camper.bathroom
+        : true;
+      const filterForm = filters.form ? filters.form === camper.form : true;
+
+      return (
+        filterLocation &&
+        filterAC &&
+        filterTransmission &&
+        filterKitchen &&
+        filterTv &&
+        filterBathroom &&
+        filterForm
+      );
       // Object.entries(filters).every(([key, value]) => camper[key] === value)
-    );
+    });
     return filteredCampers;
   }
 );
